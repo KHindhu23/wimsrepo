3fd9ff717439620b21ef1c02c6b56a7162eb2f4f
khindhu23 <hindhu0130@gmail.com>
2024-06-01 11:44:29 +0000

    Added new file Part2.c in Main branch

---
+++ Part2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+   // printf() displays the string inside quotation
+
+   printf("Hello, World!");
+
+printf("Hello, World! added in main branch");
+
+   return 0;
+
+}
